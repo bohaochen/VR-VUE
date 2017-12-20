c0ad1284dee6254f9fa4c5d550e31cfb6ab49a43
asdfgh5007 <472705331@qq.com>
2017-12-20 17:32:04 +0800

    新增图像识别页面

--- src/views/home/ImageRecognition.vue
+++ src/views/home/ImageRecognition.vue
@@ -3,12 +3,18 @@
 		<div class="bg"></div>
 		<img src="../../../static/img/pn_logo_03.png" class="logo" />
 
-		<div>
-			
+		<div class="album">
+			<img class="waikuang" src="../../../static/img/renlianshibiekuangdeng.png" />
+			<div class="album-content">
+				<div class="title">点击上传个人正面自拍照</div>
+				<div>
+					<input type="file" name="upload" accept="image/*">
+				</div>
+			</div>
 		</div>
 
 		<div class="btns">
-			<img src="../../../static/img/btn_04.png" @click="mashangtansuo" class="btn1" />
+			<img src="../../../static/img/querenceshi.png" @click="mashangtansuo" class="btn1" />
 		</div>
 	</div>
 </template>
@@ -45,6 +51,38 @@
 			position: absolute;
 			top: 0;
 			z-index: 1;
+		}
+		.album {
+			width: 100%;
+			position: absolute;
+			top: 12%;
+			z-index: 4;
+			.album-content {
+				position: absolute;
+				/*border: 1px solid #7EF9FF;*/
+				height: 70%;
+				margin-left: -28.25%;
+				width: 56%;
+				top: 15%;
+				left: 50%;
+				overflow: hidden;
+				background: url(../../../static/img/renlianshibierenlian.png) no-repeat 50%/cover;
+				display: -webkit-flex;
+				display: flex;
+				flex-wrap: nowrap;
+				justify-content: center;
+				align-items: center;
+				font-size: 32px;
+				.title{
+					color: #7EF9FF;
+				}
+			}
+			
+			.waikuang {
+				margin: auto;
+				display: block;
+				width: 80%;
+			}
 		}
 		.btns {
 			width: 100%;
