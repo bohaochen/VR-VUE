40bdbe76166372450e6bff2cb9734d400f995bb8
bohaochen <360485406@qq.com>
2017-12-22 01:54:00 +0800

    完善打分流程

--- src/views/home/ImageRecognition.vue
+++ src/views/home/ImageRecognition.vue
@@ -4,12 +4,15 @@
 		<img src="../../../static/img/pn_logo_03.png" class="logo" />
 
 		<div class="album">
-			<div class="score score_1">1111</div>
-			<div class="score score_2">111</div>
-			<div class="score score_3">1111</div>
+			<div v-show="isRead" class="score_box animated">
+				<div class="score score_1">1111</div>
+				<div class="score score_2">111</div>
+				<div class="score score_3">1111</div>
+			</div>
+			
 			<img class="waikuang" src="../../../static/img/renlianshibiekuangdeng.png" />
 			<div class="album-content">
-				<div class="title">点击上传个人正面自拍照</div>
+				<div class="title" v-show="isUnUpload">点击上传个人正面自拍照</div>
 				<img src="" ref="imgsss" class="imgsss" />
 				<div class="upload">
 					<input ref="upload" @change="start_upload(this)" type="file" name="upload" accept="image/*">
@@ -20,21 +23,20 @@
 			</div>
 		</div>
 
-		<div class="btns" v-show="!isUpload">
+		<div class="btns" v-show="isUnUpload">
 			<img src="../../../static/img/querenceshi.png" @click="mashangtansuo" class="btn1" />
 		</div>
 
-		<div class="sb-btn" v-show="isUpload">
+		<div class="sb-btn animated" v-show="isUpload">
 			<img src="../../../static/img/titlebg.png" class="btn1" />
 			<span class="is-cs">正在启动分析...</span>
 		</div>
 
-		<div class="sb-btn" v-show="isRead">
+		<div class="sb-btn animated" v-show="isRead">
 			<img src="../../../static/img/titlebg1.png" class="btn1" />
-			<span class="is-read">神秘指数：</span>
-			<span class="stars">
-				
-			</span>
+			<div class="star" >
+				<img class="starImg" src="../../../static/img/xingxing.png" >
+			</div>
 		</div>
 
 	</div>
@@ -45,38 +47,202 @@ export default {
   data() {
     return {
       quality: 0.8,
+      isUnUpload: true,
       isUpload: false,
-      isRead: true,
-      base64Img: null
+      isRead: false,
+      base64Img: null,
+      scoreArray: [
+        {
+          minScore: 0,
+          maxScore: 0.1,
+          starNum: 5,
+          starMsg: ["小心谨慎", "和蔼可亲", "多重性格"]
+        },
+        {
+          minScore: 0.1,
+          maxScore: 0.12,
+          starNum: 1,
+          starMsg: ["表里如一", "心境纯粹", "大智若愚"]
+        },
+        {
+          minScore: 0.12,
+          maxScore: 0.14,
+          starNum: 2,
+          starMsg: ["乐天达观", "善解人意", "聪明好学"]
+        },
+        {
+          minScore: 0.14,
+          maxScore: 0.15,
+          starNum: 3,
+          starMsg: ["风趣幽默", "感情用事", "豪放不羁"]
+        },
+        {
+          minScore: 0.15,
+          maxScore: 0.17,
+          starNum: 4,
+          starMsg: ["外表冷漠", "内心善良", "富正义感"]
+        },
+        {
+          minScore: 0.17,
+          maxScore: 0.19,
+          starNum: 5,
+          starMsg: ["深谋远虑", "洞察锐利", "占有欲强"]
+        },
+        {
+          minScore: 0.19,
+          maxScore: 0.2,
+          starNum: 1,
+          starMsg: ["纯净质朴", "内心平和", "向往自由"]
+        },
+        {
+          minScore: 0.2,
+          maxScore: 0.22,
+          starNum: 2,
+          starMsg: ["感情脆弱", "同理心重", "不拘小节"]
+        },
+        {
+          minScore: 0.22,
+          maxScore: 0.24,
+          starNum: 3,
+          starMsg: ["临机应变", "充满活力", "富于童心"]
+        },
+        {
+          minScore: 0.24,
+          maxScore: 0.25,
+          starNum: 4,
+          starMsg: ["谦逊有礼", "内心丰富", "不善表达"]
+        },
+        {
+          minScore: 0.25,
+          maxScore: 0.27,
+          starNum: 5,
+          starMsg: ["聪明机警", "善于推理", "追求挑战"]
+        },
+        {
+          minScore: 0.27,
+          maxScore: 0.29,
+          starNum: 1,
+          starMsg: ["精力充沛", "崇尚调和", "理性公正"]
+        },
+        {
+          minScore: 0.29,
+          maxScore: 0.3,
+          starNum: 2,
+          starMsg: ["坦白率真", "不受束缚", "富有野心"]
+        },
+        {
+          minScore: 0.3,
+          maxScore: 0.32,
+          starNum: 3,
+          starMsg: ["足智多谋", "好奇心强", "能屈能伸"]
+        },
+        {
+          minScore: 0.32,
+          maxScore: 0.34,
+          starNum: 4,
+          starMsg: ["善于倾听", "捉摸不定", "沉默寡言"]
+        },
+        {
+          minScore: 0.34,
+          maxScore: 0.35,
+          starNum: 5,
+          starMsg: ["自尊心强", "条理清晰", "极富野心"]
+        },
+        {
+          minScore: 0.35,
+          maxScore: 0.37,
+          starNum: 1,
+          starMsg: ["兴趣广泛", "脚踏实地", "个性单纯"]
+        },
+        {
+          minScore: 0.37,
+          maxScore: 0.39,
+          starNum: 2,
+          starMsg: ["勤奋努力", "个性明朗", "亲切温暖"]
+        },
+        {
+          minScore: 0.39,
+          maxScore: 0.4,
+          starNum: 5,
+          starMsg: ["谨慎谦虚", "创意十足", "眼光敏锐"]
+        },
+        {
+          minScore: 0.4,
+          maxScore: 100,
+          starNum: 4,
+          starMsg: ["色彩敏感", "细腻入微", "情绪不定"]
+        }
+      ]
     };
   },
   mounted() {
     let self = this;
     //			self.$refs.upload.addEventListener('change',function(){
     //
-    //			},false)
+	//			},false)
+	console.log(document.body.clientWidth*74/100)
+	document.getElementsByClassName("starImg")[0].style.width = (document.body.clientWidth*74/100 + "px");
   },
   methods: {
     mashangtansuo() {
       //马上探索
       let self = this;
       let base64Img = self.base64Img;
-      self.$http
-        .post("/user" + "?uid=" + 111 + "&action=" + "compare", {
+      self.isUnUpload = false; //初始界面消失
+	  self.isUpload = true; //出现扫描界面
+      self.$http.post("/user" + "?uid=" + 111 + "&action=" + "compare", {
           base64Img
         })
         .then(function(response) {
           console.log(response);
-          alert("图片上传成功");
+          if (response) {
+            self.score(response); //设置打分界面
+          }
         })
         .catch(function(error) {
           console.log(error);
-          self.isUpload = true;
+          self.score();
         });
     },
-    score() {
-      self.isUpload = true;
-      alert(1);
+    score(obj) {
+      var self = this;
+      var score = 0.2; //需要改成OBJ的分数值！！！！！！！！！！！！！！！！！！！！！！！
+      var scoreIndex = null;
+      setTimeout(function() {
+        self.isUpload = false; //扫描消失
+        self.isRead = true; //分数出现
+        self.scoreArray.forEach((element, index) => {
+          if (score >= element.minScore && score < element.maxScore) {
+            scoreIndex = index;
+          }
+        });
+        document.getElementsByClassName("score_1")[0].innerHTML =
+          self.scoreArray[scoreIndex].starMsg[0];
+        document.getElementsByClassName("score_2")[0].innerHTML =
+          self.scoreArray[scoreIndex].starMsg[1];
+        document.getElementsByClassName("score_3")[0].innerHTML =
+          self.scoreArray[scoreIndex].starMsg[2];
+        switch (self.scoreArray[scoreIndex].starNum) {
+          case 1:
+            document.getElementsByClassName("star")[0].style.width = "53%";
+            break;
+          case 2:
+            document.getElementsByClassName("star")[0].style.width = "64%";
+            break;
+          case 3:
+            document.getElementsByClassName("star")[0].style.width = "74%";
+            break;
+          case 4:
+            document.getElementsByClassName("star")[0].style.width = "85%";
+            break;
+          case 5:
+            document.getElementsByClassName("star")[0].style.width = "100%";
+            break;
+
+          default:
+            break;
+        }
+      }, 2000);
     },
     start_upload(obj) {
       let self = this;
@@ -240,7 +406,24 @@ export default {
     top: 0;
     z-index: 1;
   }
-
+  .score_box {
+    animation: fuxian 0.5s linear 0.5s;
+	opacity: 0;
+    z-index: 10;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  .stars {
+    position: absolute;
+    top: 50%;
+    margin-top: -25px;
+    left: 239px;
+    height: 56px;
+    display: block;
+    width: 340px;
+    overflow: hidden;
+  }
   .album {
     width: 100%;
     position: absolute;
@@ -333,12 +516,28 @@ export default {
     position: absolute;
     z-index: 4;
     top: 70%;
+	opacity: 0;
     margin-top: -40px;
     left: 13%;
+    animation: fuxian 0.5s linear;
     .btn1 {
       width: 100%;
       display: block;
       margin: auto;
+    }
+    .star {
+      width: 100%;
+      height: 100%;
+      padding: 0px;
+      overflow: hidden;
+      position: absolute;
+      top: 0px;
+      box-sizing: border-box;
+      img {
+        position: relative;
+		top: 50%;
+		transform: translate(0,-50%);
+      }
     }
     .is-cs {
       position: absolute;
@@ -379,6 +578,7 @@ export default {
     overflow: hidden;
     img {
       top: 0px;
+      width: 100%;
       position: absolute;
     }
   }
@@ -392,6 +592,17 @@ export default {
     to {
       top: 100%;
     }
+  }
+  @keyframes fuxian {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  .animated{
+	  animation-fill-mode: forwards;
   }
 }
 </style>
