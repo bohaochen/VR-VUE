55907d8ec7c84f72ba39479cc2a60105630cf102
Administrator <Administrator@B2C2J0E2QUF7RNS>
2017-12-23 13:27:02 +0800

    发布外网

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -50,11 +50,13 @@
 				let self = this;
 				let url = window.location.href.split('?')[0];
 				let code = self.$route.query.code;
+				alert(code);
 				if(code == null || code == '' || !code) {
 					window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx42c67be9af7fa426&redirect_uri=' + encodeURIComponent(url) + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
 				} else {
 					self.$http.post("v1/em?action=oauth2&code=" + code, {})
 						.then(function(response) {
+							alert("code：" + response.data.code);
 							if(response.data.code == 200) {
 								console.log(response.data.openid); //用户ID
 								window.localStorage.setItem("openid", response.data.openid);
